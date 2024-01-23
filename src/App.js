8212122e94f3deb354aaaedeb779cf2dc35419ad
kanishkadeo27 <Kanishkadeo92022@gmail.com>
2024-01-23 17:11:13 +0530

    game done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Slot from "./Components/Slot";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="heading_style">
+        🎰 Welcome to{" "}
+        <span style={{ fontWeight: "bold" }}>Slot Machine Game</span>🎰
+      </h1>
+
+      <div className="slotmachine">
+        <Slot  x = "😄" y = "😄"  z = "😄" />
+        <Slot x = "😄" y = "😉"  z = "😊" />
+        <Slot x = "🍎" y = "🍌"  z = "🍎" /> 
+      </div>
+    </>
   );
 }
 
